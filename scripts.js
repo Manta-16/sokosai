04c0fb17b19d0ada78a7201fb858d0f2622c10a4
Manta-16 <shgknd.otsok_94@au.com>
2025-09-05 17:44:22 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,44 @@
+function play_sound_level() {
+  const audio = new Audio("レベルアップ.mp3");
+  audio.play();
+}
+function play_sound_doramu() {
+  const audio = new Audio("ドラムロール.mp3");
+  audio.play();
+}
+function play_sound_jaja_n() {
+  const audio = new Audio("ジャジャーン.mp3");
+  audio.play();
+}
+function play_sound_yeah() {
+  const audio = new Audio("男衆「イエーイ！」.mp3");
+  audio.play();
+}
+function play_sound_donpafu() {
+  const audio = new Audio("ドンドンパフパフ.mp3");
+  audio.play();
+}
+function play_sound_ramen() {
+  const audio = new Audio("ラーメン屋台登場.mp3");
+  audio.play();
+}
+function play_sound_fuan() {
+  const audio = new Audio("不安（ピアノ演奏）.mp3");
+  audio.play();
+}
+function play_sound_hyousigi() {
+  const audio = new Audio("拍子木3.mp3");
+  audio.play();
+}
+function play_sound_kansei() {
+  const audio = new Audio("歓声と拍手.mp3");
+  audio.play();
+}
+function play_sound_stadium() {
+  const audio = new Audio("スタジアムの歓声.mp3");
+  audio.play();
+}
+function play_sound_horagai() {
+  const audio = new Audio("ほら貝を吹き鳴らす.mp3");
+  audio.play();
+}
