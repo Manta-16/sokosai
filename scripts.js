e0a642b279b2877a7973e083a03accb0f024dae3
Manta-16 <shgknd.otsok_94@au.com>
2025-09-05 19:22:47 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,42 +3,42 @@ function play_sound_level() {
   audio.play();
 }
 function play_sound_doramu() {
-  const audio = new Audio("ドラムロール.mp3");
+  const audio = new Audio("dramroll.mp3");
   audio.play();
 }
 function play_sound_jaja_n() {
-  const audio = new Audio("ジャジャーン.mp3");
+  const audio = new Audio("jajan.mp3");
   audio.play();
 }
 function play_sound_yeah() {
-  const audio = new Audio("男衆「イエーイ！」.mp3");
+  const audio = new Audio("yeah.mp3");
   audio.play();
 }
 function play_sound_donpafu() {
-  const audio = new Audio("ドンドンパフパフ.mp3");
+  const audio = new Audio("donpafu.mp3");
   audio.play();
 }
 function play_sound_ramen() {
-  const audio = new Audio("ラーメン屋台登場.mp3");
+  const audio = new Audio("ramen.mp3");
   audio.play();
 }
 function play_sound_fuan() {
-  const audio = new Audio("不安（ピアノ演奏）.mp3");
+  const audio = new Audio("fuan.mp3");
   audio.play();
 }
 function play_sound_hyousigi() {
-  const audio = new Audio("拍子木3.mp3");
+  const audio = new Audio("hyousigi3.mp3");
   audio.play();
 }
 function play_sound_kansei() {
-  const audio = new Audio("歓声と拍手.mp3");
+  const audio = new Audio("kansei.mp3");
   audio.play();
 }
 function play_sound_stadium() {
-  const audio = new Audio("スタジアムの歓声.mp3");
+  const audio = new Audio("stadium.mp3");
   audio.play();
 }
 function play_sound_horagai() {
-  const audio = new Audio("ほら貝を吹き鳴らす.mp3");
+  const audio = new Audio("horagai.mp3");
   audio.play();
 }
